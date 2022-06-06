86a602859a23c8fdc313fbc27f93885e4c00c6b4
aniffer <106635485+aniffer@users.noreply.github.com>
2022-06-05 23:32:48 -0600

    Add files via upload

---
+++ Investigacion/src/main/java/com/sc202/investigacion/Investigacion.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sc202.investigacion;
+
+/**
+ *
+ * @author Anifer
+ */
+public class Investigacion {
+
+    public static void main(String[] args) {
+        //1.String.toLowerCase() 
+        
+     //    public String toLowerCase()
+    //var textoMayusculas="ANIFFER";
+   //document.write(textoMayusculas.toLowerCase());
+           
+     //2. toUpperCase () 
+    // String S1 = new String("aniffer");  
+   //   System.out.println(S1.toUpperCase()); 
+     
+   //3. String.equals()  
+    
+//String sCadena1 = new String("Chacon"); 
+//String sCadena2 = new String("Luna"); 
+//String sCadena3 = new String("Chacon"); 
+		 
+//if (sCadena1.equals(sCadena2)) 
+// System.out.println(sCadena1 + " y " + sCadena2 + " son IGUALES"); 
+//else 
+// System.out.println(sCadena1 + " y " + sCadena2 + " son DIFERENTES"); 
+	 
+//if (sCadena1.equals(sCadena3)) 
+// System.out.println(sCadena1 + " y " + sCadena3 + " son IGUALES"); 
+//else 
+// System.out.println(sCadena1 + " y " + sCadena3 + " son DIFERENTES"); 
+   
+
+//
+        String str_Sample = "a";
+        System.out.println("Compare To 'a' b is : " + str_Sample.compareTo("b"));
+        str_Sample = "b";
+        System.out.println("Compare To 'b' a is : " + str_Sample.compareTo("a"));
+        str_Sample = "b";
+        System.out.println("Compare To 'b' b is : " + str_Sample.compareTo("b"));
+   
+     //5.String.trim()  
+    //String aCadena = " esto es un ejemplo de cadena" ; 
+   //System.out.println(aCadena.trim()); 
+    
+    
+    }
+}
